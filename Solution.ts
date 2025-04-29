0109a11b939fa4c4b394b350f54c0a8a818ea271
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-04-29 10:24:17 +0200

    Create Solution.ts

---
+++ Solution.ts
@@ -0,0 +1,13 @@
+
+function maximumTripletValue(input: number[]): number {
+    let maxTripletValue = 0;
+    let maxFirstOperand = 0;
+    let maxDifferenceFirstMinusSecondOperands = 0;
+
+    for (let i = 0; i < input.length; ++i) {
+        maxTripletValue = Math.max(maxTripletValue, maxDifferenceFirstMinusSecondOperands * input[i]);
+        maxFirstOperand = Math.max(maxFirstOperand, input[i]);
+        maxDifferenceFirstMinusSecondOperands = Math.max(maxDifferenceFirstMinusSecondOperands, maxFirstOperand - input[i]);
+    }
+    return maxTripletValue;
+};
